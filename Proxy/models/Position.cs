1889078c37ecaec781d8055885c3d5241f02ded3
Romain Baudet <romain74.baudet@gmail.com>
2022-03-18 11:03:23 +0100

    stations retrieved at launch

---
+++ Proxy/models/Position.cs
@@ -0,0 +1,7 @@
+﻿namespace Proxy.models;
+
+public class Position
+{
+    public double lat { get; set; }
+    public double lng { get; set; }
+}
---
+++ Proxy/models/Station.cs
@@ -0,0 +1,17 @@
+﻿namespace Proxy.models;
+
+public class Station
+{
+    public int number { get; set; }
+    public string contract_name { get; set; }
+    public string name { get; set; }
+    public string address { get; set; }
+    public Position position { get; set; }
+    public bool banking { get; set; }
+    public bool bonus { get; set; }
+    public int bike_stands { get; set; }
+    public int available_bike_stands { get; set; }
+    public int available_bikes { get; set; }
+    public string status { get; set; }
+    public long last_update { get; set; }
+}
