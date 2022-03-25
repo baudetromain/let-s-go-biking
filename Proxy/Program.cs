2b54af9060864e10440455b222ccb3d06833b701
Natsukooh <romain74.baudet@gmail.com>
2022-03-25 09:58:43 +0100

    moved main code in other project cause proxy wans't supposed to handle it

--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -1,63 +1,7 @@
-﻿using System.Diagnostics;
-using System.Net;
-using System.Text.Json;
-using Proxy.models;
-
-namespace Proxy
+﻿namespace Proxy
 {
     internal class Proxy
     {
-        private static HttpListener httpListener = new HttpListener();
-        private static HttpClient httpClient = new HttpClient();
-        private static string api_key = "e0beee5c2f1a0468494223125c4c35b1761aaffc";
-        private static List<Station> stations = new List<Station>();
         
-        private static async Task Main(string[] args)
-        {
-            Console.WriteLine("Initiating the proxy...");
-            
-            Console.Write("Retrieving existing stations... ");
-            try
-            {
-                await retrieveStations();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("\nAn error occured while trying to retrieve the list of existing stations.");
-                System.Environment.Exit(1);
-            }
-        }
-
-        private static async Task retrieveStations()
-        {
-            Stopwatch clock = Stopwatch.StartNew();
-            
-            HttpResponseMessage response = await httpClient.GetAsync($"https://api.jcdecaux.com/vls/v1/stations?apiKey={api_key}");
-            response.EnsureSuccessStatusCode();
-            var responseBody = await response.Content.ReadAsStringAsync();
-            stations = JsonSerializer.Deserialize<List<Station>>(responseBody);
-            
-            clock.Stop();
-            Console.WriteLine($"Retrieved {stations.Count} stations in {clock.Elapsed.Milliseconds}ms !");
-        }
-
-        private static Station getClosestStation(double lat, double lng)
-        {
-            Station closest_station = null;
-            double best_distance = -1;
-            
-            foreach (var station in stations)
-            {
-                double distance = station.distance_to(lat, lng);
-                
-                if (best_distance < 0 || distance < best_distance)
-                {
-                    closest_station = station;
-                    best_distance = distance;
-                }
-            }
-
-            return closest_station;
-        }
     }
 }
--- RoutingServer/models/Position.cs
+++ RoutingServer/models/Position.cs
@@ -1,4 +1,4 @@
-﻿namespace Proxy.models;
+﻿namespace RoutingServer.models;
 
 public class Position
 {
